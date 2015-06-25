1c4670d1bb6c0fba27668f0027a1ffb3e7928273
tim <tim@magnetic.io>
2015-06-25 13:25:41 +0200

    fixed start script

--- src/main/resources/start.sh
+++ src/main/resources/start.sh
@@ -1,3 +1,3 @@
 # dump envvars:
 env
-cd dropwizardtest-1.0-SNAPSHOT && java -jar dropwizardtest-1.0-SNAPSHOT.jar server helloworld.yaml
+java -jar dropwizardtest-1.0-SNAPSHOT.jar server helloworld.yaml
