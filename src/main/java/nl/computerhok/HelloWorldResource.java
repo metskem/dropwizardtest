0bbab37e881df3136d6bf40c623abf456592834a
Harry Metske <h.metske@rn.rabobank.nl>
2015-09-14 14:13:54 +0200

    provide server-info service

--- src/main/java/nl/computerhok/HelloWorldResource.java
+++ src/main/java/nl/computerhok/HelloWorldResource.java
@@ -75,7 +75,7 @@ public class HelloWorldResource {
     @Path("/server-info")
     public String server_info(@Context HttpServletRequest request) {
         StringBuilder resp = new StringBuilder();
-        resp.append("\napplication version   : 1.5");
+        resp.append("\napplication version   : 1.6");
         resp.append("\nserver time           : " + new LocalDateTime());
         resp.append("\ninstance start time   : " + startTime);
         resp.append("\ninstance hitcount     : " + ++simpleHitCounter);
